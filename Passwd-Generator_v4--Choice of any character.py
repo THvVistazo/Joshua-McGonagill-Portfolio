7f79a3925d8418e5fc1934c7ca1c952399852a15
Joshua McGonagill <83095388+THvVistazo@users.noreply.github.com>
2022-12-15 15:09:20 -0700

    Passwd-Gen_Choice_of_Characters
    This program written in python took me a little while to write. After writing 4 different versions, I am pleased to present this simple, random password generator. This version of my password generator will allow you to choose the quantity of letters, numbers and generally accepted special characters. The total length of your password will be printed below your generated password.

---
+++ Passwd-Generator_v4--Choice of any character.py
@@ -0,0 +1,51 @@
+# Created with Pyto
+
+
+
+
+import string
+import random
+
+
+# characters to generate password from
+letters = list(string.ascii_letters)
+numbers = list(string.digits)
+special_characters = list("!@#$%^&*" + "()")
+
+def generate_random_password():
+    # length of password from the user
+    letter_count = int(input("Enter the desired amount of letters: "))
+
+    # amount of numbers in password
+    number_count= int(input("Enter the desired number count: "))
+
+    # amount of special characters in password
+    spec_char_count = int(input("Enter the desired special character count: "))
+
+    # picking random characters from the list
+    password = []
+    for i in range(letter_count):
+        password.append(random.choice(letters))
+
+    for i in range(number_count):
+        password.append(random.choice(numbers))
+    
+    for i in range(spec_char_count):
+        password.append(random.choice(special_characters))
+
+
+    # shuffling the resultant password
+    random.shuffle(password)
+
+	
+	# printing the list
+    print("".join(password))
+
+    length = str(letter_count + number_count + spec_char_count)
+
+    print("Your password is " + length + " characters long.")
+
+
+## invoking the function
+generate_random_password()
+
