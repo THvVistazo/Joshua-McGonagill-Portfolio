d288cbbfb60f9d7c40103ab78d7031a7838db30c
Joshua McGonagill <83095388+THvVistazo@users.noreply.github.com>
2023-04-12 07:44:01 -0600

    Password Generator with true Entropy
    Security fixes: input validation and secure random generation of characters.

--- Passwd-Generator_v4--Choice of any character.py
+++ Passwd-Generator_v4--Choice of any character.py
@@ -1,7 +1,6 @@
-
+import secrets
 import string
 import random
-
 
 # characters to generate password from
 letters = list(string.ascii_letters)
@@ -9,39 +8,65 @@ numbers = list(string.digits)
 special_characters = list("!@#$%^&*" + "()")
 
 def generate_random_password():
-    # amount of letters in password
-    letter_count = int(input("Enter the desired amount of letters: "))
+		# amount of letters in password with input validation
+		while True:
+			try:
+				letter_count = int(input("Enter the desired amount of letters: "))
+			except ValueError:
+				print("Please use numbers only. No spaces, no special characters- just numbers.")
+				continue
+			else:
+				break
+			# amount of numbers in password
+		while True:		
+			try:
+				number_count= int(input("Enter the desired amount of numbers: "))
+			except ValueError:
+				print("Please use numbers only. No spaces, no special characters- just numbers.")
+				continue
+			else:
+				break
+			# amount of special characters in password
+		while True:	
+			try:
+				spec_char_count = int(input("Enter the desired amount of characters: "))
+			except ValueError:
+				print("Please use numbers only. No spaces, no special characters- just numbers.")
+				continue
+			else:
+				break
+		# picking random characters from the list
+		password = []
+		for i in range(letter_count):
+			password.append(secrets.choice(letters))
 
-    # amount of numbers in password
-    number_count= int(input("Enter the desired amount of numbers: "))
+		for i in range(number_count):
+			password.append(secrets.choice(numbers))
+		
+		for i in range(spec_char_count):
+			password.append(secrets.choice(special_characters))
 
-    # amount of special characters in password
-    spec_char_count = int(input("Enter the desired amount of characters: "))
+		# shuffling the resultant password
+		random.shuffle(password)
 
-    # picking random characters from the list
-    password = []
-    for i in range(letter_count):
-        password.append(random.choice(letters))
+		
+		# printing the list
+		print("".join(password))
 
-    for i in range(number_count):
-        password.append(random.choice(numbers))
-    
-    for i in range(spec_char_count):
-        password.append(random.choice(special_characters))
+		length = str(letter_count + number_count + spec_char_count)
 
-
-    # shuffling the resultant password
-    random.shuffle(password)
-
-	
-	# printing the list
-    print("".join(password))
-
-    length = str(letter_count + number_count + spec_char_count)
-
-    print("Your password is " + length + " characters long.")
+		print("Your password is " + length + " characters long.")
 
 
-## invoking the function
+## invoking the function with error handling
 generate_random_password()
 
+
+""" Accredited links:
+		Input validation and loop:
+  			https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
+		Entropy:
+			https://docs.python.org/3/library/secrets.html
+		Password generator base idea:
+			https://gist.github.com/23maverick23/4131896
+"""
